8797658965194581f604737d6527ebc5d1caf4c3
CefasvanEck <cefasvaneck@gmail.com>
2024-03-17 13:43:19 +0100

    Turn-Based versions + Health Scripts + New Tiles

---
+++ Assets/Scripts/Slider.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.Windows;
+using System.Text.RegularExpressions;
+
+public class SliderController : MonoBehaviour
+{
+    [SerializeField]
+    private Slider slider;
+    [SerializeField]
+    private float maxValue;
+    [SerializeField]
+    private float currentValue;
+
+    [SerializeField]
+    private TextMeshProUGUI textMeshPro;
+    [SerializeField]
+    private string textName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentValue = maxValue;
+        slider.value = currentValue;
+    }
+
+    // Function to set the slider value
+    private void SetSliderValue(float value)
+    {
+        slider.value = value;
+        currentValue = value;
+    }
+
+    void FixedUpdate()
+    {
+        SetSliderValue(currentValue - 0.01f);
+
+
+        if (textMeshPro != null)
+        {
+            string result = Regex.Replace(textMeshPro.text, @"\d", "");
+            textMeshPro.text = result;
+            textMeshPro.text = textName + (int)currentValue + "";
+        }
+    }
+
+    public float getValue()
+    {
+        return currentValue;
+    }
+}
