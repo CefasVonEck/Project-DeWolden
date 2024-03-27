b899ed2bcd78db9ef4003c9eb66d44e1692c98ff
MSI_KATANA\milbo <milbos21@gmail.com>
2024-03-27 11:18:49 +0100

    the scene switcher

---
+++ Assets/locationhandler.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class locationhandler : MonoBehaviour
+{
+    [Tooltip("the locationswapper the player interacted with ")]
+    public int GoLocation;
+
+    // all the locationswappers
+
+    public 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ClickYes()
+    {
+
+    }
+
+    public void ClickNo()
+    {
+
+    }
+}
--- Assets/locationswap/locationswap.cs
+++ Assets/locationswap/locationswap.cs
@@ -1,26 +1,120 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
 // Scenemanager
 
 public class locationswap : MonoBehaviour
 {
+    [Tooltip ("put the name of the scene in here")]
+    [SerializeField]
+    private string sceneName;
+
+    [SerializeField]
+    private GameObject pop;
+    [SerializeField]
+    private GameObject Over;
+
+    [SerializeField]
+    private string loc;
+
+    [SerializeField]
+    private TextMeshProUGUI Name;
+
+    [SerializeField]
+    private GameObject Play;
+
+    [SerializeField]
+    private float moveSpeed;
+
+    public Vector2 Direction;
+
+    [SerializeField]
+    private bool canmove;
+
+    [SerializeField]
+    private bool enteredTrigger;
+
+    private void Awake()
+    {
+        pop = GameObject.FindWithTag("locpop");
+        Over = GameObject.FindWithTag("Over");
+        Play = GameObject.FindWithTag("Player");
+        pop.SetActive(false);
+        Over.SetActive(false);
+        canmove = false;
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (canmove == true)
+        {
+            //move the player out of the scene
+            Play.transform.Translate(moveSpeed * Time.deltaTime * Direction);
+        }
+
+        if (enteredTrigger == true)
+        {
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                clickyes();
+            }
+            else if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                clickno();
+            }
+        }
     }
 
     // during collision
-        // show pop up
-            // if clicked yes move to location
-                // move the player and side character out of the scene 
-                    // wait till they are out of the scene and show a black overlay
-                        // change the scene
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            // show pop up
+            pop.SetActive(true);
+            Time.timeScale = 0f;
+
+            Name.text = loc.ToString();
+
+            enteredTrigger = true;
+        }
+    }
+
+    // if clicked yes move to location
+    public void clickyes()
+    {
+        Time.timeScale = 1f;
+
+        //show a black overlay
+        Over.SetActive(true);
+        pop.SetActive(false);        
+        canmove = true;
+        Invoke("changescene", 2);
+        Invoke("Res", 1);
+    }
+
+    public void clickno()
+    {
+        Time.timeScale = 1f;
+        pop.SetActive(false);
+    }
+
+    public void changescene()
+    {
+        // change the scene
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void Res()
+    {
+        canmove = false;
+    }
 }
